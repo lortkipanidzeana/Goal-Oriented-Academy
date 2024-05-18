8078bd6a47ee5e870f72c3b3f6d87c03811fbb55
lortkipanidzeana <lortkipanidzeeana@gmail.com>
2024-05-18 15:22:34 +0400

    commit

---
+++ Level 03/Level03.py
@@ -0,0 +1,10 @@
+#1
+me="ana lortkipanidze"
+deda="natia azmaiparashvili"
+mama="nika lortkipanidze"
+dzma="dimitri lortkimanidze"
+print(me + " " + deda)
+print(mama + " " + dzma)
+
+#2
+print()
---
+++ Level 04/Level04.py
@@ -0,0 +1,32 @@
+"""შექმენით პითონის ფაილი და დაპრინტეთ თქვენი სახელი,
+გვარი და ასაკი, ასევე ოჯახის წევრების სახელი, გვარი და ასაკი"""
+
+family="ana lortkipanidze 11 wlis,natia azmaiparashvili 33 wlis,niko lortkipanidze 34 wlis,dimitri lortkipanidze 14 wlis"
+print(family)
+
+
+
+
+
+
+#3
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+print(11)
+print(12)
+print(13)
+print(14)
+print(15)
+print(16)
+print(17)
+print(18)
+print(19)
+print(20)
